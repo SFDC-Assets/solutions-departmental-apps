59fbdbdeedb5e5058302b5cfa5eeffcbb815caee
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 16:29:37 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,21 +1,8 @@
-# Create the demo org (uncomment for local development)
-# sfdx force:org:delete -u departmental-apps
-# sfdx force:org:create -f config/project-scratch-def.json --setalias departmental-apps --setdefaultusername
-
-# Create the demo org (uncomment for the SFDX scratch org deployer)
-sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s --wait 60 --userprefix admin -o electron.demo
-
-# Push the metadata into the new scratch org.
-sfdx force:source:push
-
-# Assign the permset to the default admin user.
-sfdx force:user:permset:assign -n electron
-
-# Assign a special analytics permset to the Integration User used by Einstein Analytics
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p admin -e electron.demo
+sf project deploy start
+sf org assign permset -n electron
 sfdx shane:user:permset:assign -n analytics -g Integration -l User
-
-# Import the data required by the demo
-sfdx automig:load --inputdir ./data --deletebeforeload
+sf automig load --inputdir ./data --deletebeforeload
 
 # Deploy the metadata for the the dataflow (this needed to happen AFTER the other meta data was pushed and the permset was applied to the Integration user)
 sfdx force:source:deploy -p dataflow
@@ -25,12 +12,6 @@ sfdx shane:analytics:dataflow:start -n Electron
 
 # Deploy the metadata for the Einstein Analytics visualizations (this will only deploy after the dataflow has run)
 sfdx force:source:deploy -p visualizations
-
-# Activate the custom theme.
 sfdx shane:theme:activate -n Electron
-
-# Set the default password.
-sfdx shane:user:password:set -g User -l User -p sfdx1234
-
-# Open the demo org.
-sfdx force:org:open
+sf demoutil user password set -p salesforce1 -g User -l User
+sf org open
