702480eaec0192d0b43e132ca378b5c7dc59e520
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 16:30:49 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -5,13 +5,13 @@ sfdx shane:user:permset:assign -n analytics -g Integration -l User
 sf automig load --inputdir ./data --deletebeforeload
 
 # Deploy the metadata for the the dataflow (this needed to happen AFTER the other meta data was pushed and the permset was applied to the Integration user)
-sfdx force:source:deploy -p dataflow
+sf project deploy start -d dataflow
 
 # Start the dataflow for the Analytics.
 sfdx shane:analytics:dataflow:start -n Electron
 
 # Deploy the metadata for the Einstein Analytics visualizations (this will only deploy after the dataflow has run)
-sfdx force:source:deploy -p visualizations
+sf project deploy start -d visualizations
 sfdx shane:theme:activate -n Electron
 sf demoutil user password set -p salesforce1 -g User -l User
 sf org open
